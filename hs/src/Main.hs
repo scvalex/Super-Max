4f463b26bb610c6a79527b4812d7ed2d6854ba20
Francesco <f@mazzo.li>
2012-01-13 17:11:52 +0000

    Abstracted the Level

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -1,10 +1,18 @@
-module Main where
+import qualified Graphics.Gloss as G
 
-import Graphics.Gloss
-
+import Types
 import Level
 
+dummyObject :: Static
+dummyObject = Static pic (0,0) (3,1) path
+  where
+    te   :: Num a => a
+    te   = fromIntegral tileEdge
+    path :: Num a => [(a, a)]
+    path = [(0,0),(te*3,0),(te*3,-te),(0,-te)]
+    pic  = G.color G.red $ G.Polygon path
+
 main :: IO ()
-main = do
-    pic <- loadBMP "./resources/test.bmp"
-    display (InWindow "SuperMax" (1024, 768) (10, 10)) black $ drawLevel (dummyLevel pic)
+main =
+    G.display (G.InWindow "SuperMax" (1024, 768) (10, 10)) G.black $
+    drawLevel (buildLevel [LevelObject dummyObject])
