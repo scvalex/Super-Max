37ac493efc0d47fd89e4b89952323339aebea3d6
Alexandru Scvortov <scvalex@gmail.com>
2013-02-07 03:37:01 +0000

    use gloss

--- blind-horror/src/blind-horror.hs
+++ blind-horror/src/blind-horror.hs
@@ -1,7 +1,11 @@
 module Main where
 
+import Graphics.Gloss
+
 main :: IO ()
 main = do
     putStrLn "Everything is fine."
     putStrLn "There is nothing to worry about."
     putStrLn "ohgod ohgod ohgod"
+
+    play (FullScreen (1000, 1000)) black 5 () (\_ -> Circle 80) (\_ () -> ()) (\_ () -> ())
