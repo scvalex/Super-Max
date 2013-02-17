eb5e16f1d8bc9c12e44181d54898dc128e7e532e
Alexandru Scvortov <scvalex@gmail.com>
2013-02-17 02:33:15 +0000

    explain what the PQ functions do

--- blind-horror/src/Data/PriorityQueue.hs
+++ blind-horror/src/Data/PriorityQueue.hs
@@ -16,20 +16,25 @@ heap1@(SkewNode x1 l1 r1) +++ heap2@(SkewNode x2 l2 r2)
 Empty +++ heap = heap
 heap +++ Empty = heap
 
+-- | Get and remove the minimum element from the priority queue.
 extractMin :: (Ord a) => SkewHeap a -> Maybe (a, SkewHeap a)
 extractMin Empty            = Nothing
 extractMin (SkewNode x l r) = Just (x , l +++ r )
 
+-- | Get the minimum element from the priority queue, without removing it.
 peekMin :: (Ord a) => SkewHeap a -> Maybe a
 peekMin Empty            = Nothing
 peekMin (SkewNode x _ _) = Just x
 
+-- | Add an element to the priority queue.
 insert :: (Ord a) => SkewHeap a -> a -> SkewHeap a
 insert heap x = heap +++ (SkewNode x Empty Empty)
 
+-- | An empty priority queue.
 empty :: SkewHeap a
 empty = Empty
 
+-- | Is the given priority queue empty?
 isEmpty :: SkewHeap a -> Bool
 isEmpty Empty = True
 isEmpty _     = False
