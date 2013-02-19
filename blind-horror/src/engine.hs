6bbe9f3df5335ac4054495e16f81b644cff7cf93
Alexandru Scvortov <scvalex@gmail.com>
2013-02-19 01:59:44 +0000

    basic structure for SDL app

--- blind-horror/src/engine.hs
+++ blind-horror/src/engine.hs
@@ -1,5 +1,20 @@
 module Main where
 
+import Control.Concurrent ( threadDelay )
+import Graphics.UI.SDL ( InitFlag(..), withInit
+                       , Surface, SurfaceFlag(..), setVideoMode )
+
 main :: IO ()
 main = do
-    putStrLn "Ok"
+    withScreen 640 480 $ \_ -> do
+        putStrLn "Ok"
+        threadDelay 1000000
+
+withScreen :: Int                -- ^ width
+           -> Int                -- ^ height
+           -> (Surface -> IO ()) -- ^ action to run
+           -> IO ()
+withScreen w h act = do
+    withInit [InitEverything] $ do
+        s <- setVideoMode w h 32 [SWSurface]
+        act s
