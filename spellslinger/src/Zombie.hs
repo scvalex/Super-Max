7297168389d9b7df1fb45cf316b76b56d53e3680
Alexandru Scvortov <scvalex@gmail.com>
2013-03-01 04:24:34 +0000

    zombie are a pulsating green

--- spellslinger/src/Zombie.hs
+++ spellslinger/src/Zombie.hs
@@ -15,6 +15,7 @@ data Zombie = Zombie
     { getZombieId         :: EntityId
     , getZombiePosition   :: Position
     , getZombieAreaBounds :: (Int, Int, Int, Int)
+    , getZombieVisualTint :: Double
     } deriving ( Eq, Show )
 
 instance Entity Zombie where
@@ -29,19 +30,22 @@ instance Entity Zombie where
         return (Zombie { getZombieId         = zid
                        , getZombiePosition   = Position (xz, yz)
                        , getZombieAreaBounds = getAreaBounds zp
+                       , getZombieVisualTint = pi
                        })
 
     eid (Zombie { getZombieId = zid }) = zid
 
     position (Zombie { getZombiePosition = pos }) = pos
 
-    draw (Zombie { getZombiePosition = Position (xz, yz)
-                 , getZombieAreaBounds = bounds }) =
+    draw (Zombie { getZombiePosition   = Position (xz, yz)
+                 , getZombieAreaBounds = bounds
+                 , getZombieVisualTint = tint }) =
         fromAreaCoordinates bounds $
-        Colour (RGBA 255 0 0 255) $
+        Colour (RGBA 0 (180 + floor (75.0 * sin tint)) 0 255) $
         intRectangle xz yz 1 1
 
-    tickVisual = id
+    tickVisual z@(Zombie { getZombieVisualTint = tint }) =
+        z { getZombieVisualTint = tint + pi / 16 }
 
 setPosition :: Zombie -> Position -> Zombie
 setPosition z pos = z { getZombiePosition = pos }
