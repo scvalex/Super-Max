6eac74291635cd0c7ea86fb6a87869d2b6f30f2a
Alexandru Scvortov <scvalex@gmail.com>
2013-03-02 05:44:15 +0000

    better drawing for exit

--- spellslinger/src/RoomExit.hs
+++ spellslinger/src/RoomExit.hs
@@ -36,14 +36,20 @@ instance Entity RoomExit where
     eid (RE { getREId = reid }) = reid
 
     positions (RE { getREPosition = pos@(Position (xe, ye)) }) =
-        S.fromList [pos, Position (xe + 1, ye)]
+        S.fromList [ pos, Position (xe + 1, ye)
+                   , Position (xe, ye + 1), Position (xe + 1, ye + 1)
+                   ]
 
     draw (RE { getREPosition   = Position (xe, ye)
              , getREAreaBounds = bounds }) =
         fromAreaCoordinates bounds $
+        Translate (fromIntegral xe) (fromIntegral ye) $
         mconcat [ Colour (RGBA 255 215 0 255) $
-                  intRectangle xe ye 2 1
-                , Translate (fromIntegral (xe + 1)) (fromIntegral (ye + 1)) $
+                  intRectangle 0 0 2 2
+                , Colour (RGBA 0 0 0 255) $
+                  -- FIXME Why are the proportions fsckup up here?
+                  FilledRectangle 0.2 0.2 1.7 1.5
+                , Translate 1 2 $
                   smallText CenterAligned "Exit"
                 ]
 
