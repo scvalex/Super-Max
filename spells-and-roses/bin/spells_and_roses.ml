a53535f6ef877707d2f86fbf2996bbbc73a48491
Alexandru Scvortov <scvalex@gmail.com>
2014-04-26 23:36:03 +0100

    stub spells-and-roses project

---
+++ spells-and-roses/bin/spells_and_roses.ml
@@ -0,0 +1,28 @@
+open Core.Std
+
+let run_test () =
+  Sdl.init [`VIDEO];
+  let width, height = (320, 240) in
+  let _ =
+    Sdlwindow.create2
+      ~title:"Let's try SDL2 with OCaml!"
+      ~x:`undefined ~y:`undefined ~width ~height
+      ~flags:[]
+  in
+  Sdltimer.delay 2000;
+  Sdl.quit ()
+;;
+
+let main () =
+  Command.run
+    (Command.group ~summary:"Spells and Roses"
+       [ ( "test",
+           Command.basic
+             ~summary:"system test"
+             Command.Spec.
+               ( empty )
+             run_test )
+       ])
+;;
+
+let () = Exn.handle_uncaught ~exit:true main;;
