b83d3b9bfc66017644836209fee337a323959704
Alexandru Scvortov <scvalex@gmail.com>
2014-05-01 19:49:20 +0100

    more opportunities to quit and debug

--- spells-and-roses/bin/spells_and_roses.ml
+++ spells-and-roses/bin/spells_and_roses.ml
@@ -5,34 +5,49 @@ let game_loop ~initial_state ~on_event ~on_step ~tps ~drawing_of_state ~renderer
   let rec event_loop ~state ~history ~step =
     match Sdlevent.poll_event () with
     | None ->
-      (state, history)
+      `Continue (state, history)
     | Some ev ->
+      let history = (step, ev) :: history in
       match on_event ~state ev with
       | `Quit ->
-        Sdl.quit ();
-        exit 0
+        `Quit history
       | `Continue state ->
-        event_loop ~state ~history:((step, ev) :: history) ~step
+        event_loop ~state ~history ~step
   in
   let rec step_loop ~state ~step ~game_ticks ~now =
     if Int.(game_ticks < now)
     then begin
-      let state = on_step state in
-      step_loop ~state ~step:(step + 1) ~game_ticks:(game_ticks + slice) ~now
+      let game_ticks = game_ticks + slice in
+      let step = step + 1 in
+      match on_step state with
+      | `Quit ->
+        `Quit step
+      | `Continue state ->
+        step_loop ~state ~step ~game_ticks ~now
     end else begin
-      (state, step, game_ticks)
+      `Continue (state, step, game_ticks)
     end
   in
   let rec loop ~state ~step ~history ~game_ticks =
-    let (state, history) = event_loop ~state ~history ~step in
-    let now = Sdltimer.get_ticks () in
-    let (state, step, game_ticks) = step_loop ~state ~step ~game_ticks ~now in
-    let drawing = drawing_of_state state in
-    Drawing.render drawing ~renderer;
-    Sdltimer.delay ~ms:(game_ticks - now);
-    loop ~state ~step ~history ~game_ticks
+    match event_loop ~state ~history ~step with
+    | `Quit history ->
+      (step, history)
+    | `Continue (state, history) ->
+      let now = Sdltimer.get_ticks () in
+      match step_loop ~state ~step ~game_ticks ~now with
+      | `Quit step ->
+        (step, history)
+      | `Continue (state, step, game_ticks) ->
+        let drawing = drawing_of_state state in
+        Drawing.render drawing ~renderer;
+        Sdltimer.delay ~ms:(game_ticks - now);
+        loop ~state ~step ~history ~game_ticks
   in
-  loop ~state:initial_state ~step:0 ~history:[] ~game_ticks:(Sdltimer.get_ticks ())
+  let (step, history) =
+    loop ~state:initial_state ~step:0 ~history:[] ~game_ticks:(Sdltimer.get_ticks ())
+  in
+  Printf.eprintf "Game loop finished at step %d with history %d long\n%!"
+    step (List.length history)
 ;;
 
 let run_game ~renderer ~width ~height =
@@ -46,13 +61,15 @@ let run_game ~renderer ~width ~height =
       `Continue state
   in
   let on_step state =
-    state
+    `Continue state
   in
   let tps = 30.0 in
   let drawing_of_state _state =
     Drawing.Example.rectangles ~width ~height
   in
-  game_loop ~initial_state ~on_event ~on_step ~tps ~drawing_of_state ~renderer
+  game_loop ~initial_state ~on_event ~on_step ~tps ~drawing_of_state ~renderer;
+  Sdl.quit ();
+  exit 0
 ;;
 
 let run_test () =
@@ -63,7 +80,7 @@ let run_test () =
       ~flags:[Sdlwindow.FullScreen_Desktop]
   in
   let (width, height) = Sdlwindow.get_size window in
-  Printf.printf "Window size is (%d, %d)\n" width height;
+  Printf.eprintf "Window size is (%d, %d)\n" width height;
   Sdlwindow.set_title ~window ~title:"Something romantic";
   run_game ~renderer ~width ~height
 ;;
