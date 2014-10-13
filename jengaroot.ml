006ab3d4eb5cde52378211e0241d679a7d271731
Alexandru Scvortov <scvalex@gmail.com>
2014-10-13 19:57:29 +0100

    jenga actually finishes

--- jengaroot.ml
+++ jengaroot.ml
@@ -2,13 +2,26 @@ open Core.Std let _ = _squelch_unused_module_warning_
 open Async.Std
 open Jenga_lib.Api
 
-let env =
-  Env.create
-    (fun ~dir:_ ->
-       Scheme.switch_glob
-         [])
+let recusive_default_scheme ~dir =
+  Scheme.rules [
+    Rule.default ~dir [
+      let echo str =
+        Action.shell ~dir ~prog:"/bin/echo" ~args:[str]
+      in
+      Dep.action (Dep.return (echo "Hello, Jenga"))
+    ]
+  ]
+;;
+
+let scheme ~dir =
+  Scheme.all
+    [ recusive_default_scheme ~dir
+    ]
 ;;
 
 let setup () =
+  let env =
+    Env.create (fun ~dir -> scheme ~dir)
+  in
   Deferred.return env
 ;;
