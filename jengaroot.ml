51e659443df6921f94f8db1900f31dfb2e49b0cf
Alexandru Scvortov <scvalex@gmail.com>
2014-11-02 20:30:05 +0000

    we can actually compile ml files

--- jengaroot.ml
+++ jengaroot.ml
@@ -2,7 +2,8 @@ open Core.Std let _ = _squelch_unused_module_warning_
 open Async.Std
 open Jenga_lib.Api
 
-(* let ( *>>= ) = Dep.bind;; *)
+let ( *>>= ) = Dep.bind;;
+
 let ( *>>| ) = Dep.map;;
 
 let message str =
@@ -11,6 +12,34 @@ let message str =
 
 let (^/) dir name =
   Path.relative ~dir name
+;;
+
+let basename = Path.basename;;
+
+let ocamlopt ~dir ~args =
+  Action.shell ~dir ~prog:"ocamlopt" ~args
+;;
+
+let ocamldep ~dir ~args =
+  Action.shell ~dir ~prog:"ocamldep" ~args
+;;
+
+let glob_ml ~dir =
+  Glob.create ~dir "*.ml"
+;;
+
+let non_blank str =
+  match String.strip str with
+  | "" -> false
+  | _  -> true
+;;
+
+let split_into_lines str =
+  List.filter ~f:non_blank (String.split ~on:'\n' str)
+;;
+
+let split_into_words str =
+  List.filter ~f:non_blank (String.split ~on:'\ ' str)
 ;;
 
 (* let bash ~dir command_string = *)
@@ -215,8 +244,65 @@ end
 (*   Scheme.rules_dep rules *)
 (* ;; *)
 
-let build_exe ~dir:_ ~app ~smbuild exe =
-  Dep.return (Action.save ~target:exe (app ^ (Sexp.to_string_mach (Smbuild.sexp_of_t smbuild))))
+let link_exe_rule ~dir ~app ~smbuild:_ exe =
+  let link_exe =
+    Dep.all_unit
+      [ Dep.path (dir ^/ app ^ ".cmx")
+      ; Dep.path (dir ^/ app ^ ".o")
+      ]
+    *>>| fun () ->
+    ocamlopt ~dir
+      ~args:[ app ^ ".cmx"
+            ; "-o"; basename exe
+            ]
+  in
+  Rule.create ~targets:[exe] link_exe
+;;
+
+let ocamldep_deps ~dir ~source ~target =
+  Dep.action_stdout
+    (Dep.all_unit
+       [ Dep.glob_change (glob_ml ~dir)
+       ; Dep.path source
+       ]
+     *>>| fun () ->
+     ocamldep ~dir ~args:["-native"; basename source])
+  *>>= fun deps ->
+  let dep_paths =
+    List.map (split_into_lines deps) ~f:(fun dep ->
+      match String.split dep ~on:':' with
+      | [before; after] ->
+        let before =
+          Option.value_exn (List.hd (split_into_words before))
+            ~message:("invalid ocamldep before: " ^ before)
+        in
+        let after = split_into_words after in
+        (dir ^/ before, List.map after ~f:(fun after -> dir ^/ after))
+      | _ ->
+        failwith ("invalid ocamldep output line: " ^ dep))
+  in
+  match List.Assoc.find dep_paths target with
+  | None ->
+    failwith ("missing in ocamldep output: " ^ Path.to_string target)
+  | Some paths ->
+    message ("we depend on: " ^ String.concat (List.map paths ~f:Path.to_string));
+    Dep.all_unit
+      (List.map paths ~f:(fun path -> Dep.path path))
+;;
+
+let compile_ml_rule ~dir name =
+  let cmi = dir ^/ name ^ ".cmi" in
+  let cmx = dir ^/ name ^ ".cmx" in
+  let o = dir ^/ name ^ ".o" in
+  let compile_ml =
+    let ml = dir ^/ name ^ ".ml" in
+    Dep.path ml
+    *>>= fun () ->
+    ocamldep_deps ~dir ~source:ml ~target:cmx
+    *>>| fun () ->
+    ocamlopt ~dir ~args:["-c"; basename ml]
+  in
+  Rule.create ~targets:[cmi; cmx; o] compile_ml
 ;;
 
 let app_scheme ~dir =
@@ -224,12 +310,24 @@ let app_scheme ~dir =
     let smbuild =
       Smbuild.t_of_sexp (Sexp.of_string (String.strip smbuild))
     in
-    let app = Path.basename dir in
+    let app = basename dir in
     let exe = dir ^/ app ^ ".exe" in
-    Scheme.rules
-      [ Rule.default ~dir [Dep.path exe]
-      ; Rule.create ~targets:[exe] (build_exe ~dir ~app ~smbuild exe)
-      ])
+    let app_rules =
+      Dep.glob_listing (glob_ml ~dir)
+      *>>| fun mls ->
+      let compile_ml_rules =
+        List.map mls ~f:(fun ml ->
+          let name = String.chop_suffix_exn (basename ml) ~suffix:".ml" in
+          compile_ml_rule ~dir name)
+      in
+      List.concat
+        [ [ Rule.default ~dir [Dep.path exe]
+          ; link_exe_rule ~dir ~app ~smbuild exe
+          ]
+        ; compile_ml_rules
+        ]
+    in
+    Scheme.rules_dep app_rules)
 ;;
 
 let lib_scheme ~dir:_ =
