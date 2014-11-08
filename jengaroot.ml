b48ce0993bdb2ba9833e18ae861472e2120574c4
Alexandru Scvortov <scvalex@gmail.com>
2014-11-08 11:04:18 +0000

    more type-safe lib names

--- jengaroot.ml
+++ jengaroot.ml
@@ -17,9 +17,48 @@ let (/^) = (^/);;
 
 let basename = Path.basename;;
 
+module Lib : sig
+  type t
+
+  include Comparable.S with type t := t
+  include Hashable.S with type t := t
+
+  val name : t -> string
+  val of_name : string -> t
+  val of_suffixed_name : string -> t
+
+  val dir : t -> Path.t
+
+  val suffixed : t -> string
+end = struct
+  include String_id.Make(struct let module_name = "Jengaroot.Lib" end)
+
+  let name = to_string;;
+
+  let of_name str =
+    if String.is_suffix str ~suffix:"_lib" then
+      failwith ("Lib name already suffixed: " ^ str);
+    of_string str
+  ;;
+
+  let of_suffixed_name str =
+    match String.chop_suffix str ~suffix:"_lib" with
+    | None      -> failwith ("Suffixed lib name missing suffix: " ^ str)
+    | Some name -> of_name name
+  ;;
+
+  let suffixed t =
+    to_string t ^ "_lib"
+  ;;
+
+  let dir t =
+    Path.the_root /^ "lib" /^ to_string t
+  ;;
+end
+
 module Liblinks = struct
   let lib_dir_path ~lib =
-    Path.the_root /^ "liblinks" /^ lib
+    Path.the_root /^ "liblinks" /^ Lib.suffixed lib
   ;;
 
   let deps ~suffixes libs =
@@ -28,19 +67,15 @@ module Liblinks = struct
          let dir = lib_dir_path ~lib in
          Dep.all_unit
            (List.map suffixes ~f:(fun suffix ->
-              Dep.path (dir ^/ lib ^ suffix)))))
+              Dep.path (dir ^/ Lib.suffixed lib ^ suffix)))))
   ;;
 
   let rules ~lib =
     let liblinks_dir = lib_dir_path ~lib in
-    let lib_dir =
-      match String.chop_suffix lib ~suffix:"_lib" with
-      | None     -> failwith ("Lib name does not end in _lib: " ^ lib)
-      | Some lib -> Path.the_root /^ "lib" /^ lib
-    in
     List.map [".cmx"; ".cmi"; ".cmxa"; ".a"; ".o"] ~f:(fun suffix ->
+      let file = (Lib.suffixed lib) ^ suffix in
       let link_file =
-        let source = lib_dir ^/ lib ^ suffix in
+        let source = Lib.dir lib ^/ file in
         Dep.path source
         *>>| fun () ->
         let relative_source =
@@ -49,7 +84,7 @@ module Liblinks = struct
         Action.shell ~dir:liblinks_dir
           ~prog:"ln" ~args:["-sf"; relative_source; "."]
       in
-      Rule.create ~targets:[liblinks_dir ^/ lib ^ suffix] link_file)
+      Rule.create ~targets:[liblinks_dir ^/ file] link_file)
   ;;
 
   let include_dirs ~dir libs =
@@ -67,7 +102,7 @@ let ocamlopt ~dir ~external_libraries ~for_pack ~include_dirs ~args =
   let packages_args = "-thread" :: packages in
   let pack_args =
     Option.value_map for_pack ~default:[]
-      ~f:(fun pack -> ["-for-pack"; String.capitalize pack])
+      ~f:(fun pack -> ["-for-pack"; String.capitalize (Lib.suffixed pack)])
   in
   let include_args =
     List.concat_map include_dirs ~f:(fun include_dir ->
@@ -103,7 +138,15 @@ let split_into_words str =
   List.filter ~f:non_blank (String.split ~on:'\ ' str)
 ;;
 
-module Smbuild = struct
+module Smbuild : sig
+  type t
+
+  val load : dir : Path.t -> t Dep.t
+
+  val libraries : t -> Lib.t list
+
+  val external_libraries : t -> string list
+end = struct
   type t = {
     libraries          : string list;
     external_libraries : string list;
@@ -113,6 +156,10 @@ module Smbuild = struct
     Dep.contents (dir ^/ "smbuild")
     *>>| fun smbuild ->
     t_of_sexp (Sexp.of_string (String.strip smbuild))
+  ;;
+
+  let libraries t =
+    List.map t.libraries ~f:Lib.of_name
   ;;
 end
 
@@ -161,19 +208,7 @@ let compiled_files_for ~dir names =
     ])
 ;;
 
-let toposort_deps ~dir targets =
-  Dep.glob_listing (glob_ml ~dir)
-  *>>= fun mls ->
-  Dep.all (List.map mls ~f:(fun ml -> ocamldep_deps ~dir ~source:ml))
-  *>>| fun deps ->
-  let targets = String.Set.of_list targets in
-  let deps =
-    String.Table.of_alist_exn
-      (List.filter_map (List.concat deps) ~f:(fun (target, target_deps) ->
-         if Set.mem targets target
-         then Some (target, List.filter target_deps ~f:(Set.mem targets))
-         else None))
-  in
+let toposort ~targets deps =
   let sorted_targets = Queue.create () in
   let rec loop deps =
     if Hashtbl.is_empty deps
@@ -203,21 +238,70 @@ let toposort_deps ~dir targets =
   Queue.to_list sorted_targets
 ;;
 
+let toposort_deps ~dir targets =
+  Dep.glob_listing (glob_ml ~dir)
+  *>>= fun mls ->
+  Dep.all (List.map mls ~f:(fun ml -> ocamldep_deps ~dir ~source:ml))
+  *>>| fun deps ->
+  let targets = String.Set.of_list targets in
+  let deps =
+    String.Table.of_alist_exn
+      (List.filter_map (List.concat deps) ~f:(fun (target, target_deps) ->
+         if Set.mem targets target
+         then Some (target, List.filter target_deps ~f:(Set.mem targets))
+         else None))
+  in
+  toposort ~targets deps
+;;
+
+let transitive_libraries ~libraries =
+  let rec loop ~found lib =
+    Smbuild.load ~dir:(Lib.dir lib)
+    *>>= fun smbuild ->
+    let libraries = Smbuild.libraries smbuild in
+    if List.exists libraries ~f:(List.mem found) then
+      failwith ("Dependency cycle in library deps for " ^ Lib.suffixed lib);
+    Dep.all (List.map libraries ~f:(loop ~found:(lib :: found)))
+    *>>| fun lib_deps ->
+    (lib :: List.concat lib_deps)
+  in
+  Dep.List.concat_map libraries ~f:(loop ~found:[])
+  *>>| fun libs ->
+  Lib.(Set.to_list (Set.of_list libs))
+;;
+
+let library_deps ~libraries =
+  Dep.all
+    (List.map libraries ~f:(fun lib ->
+       Smbuild.load ~dir:(Lib.dir lib)
+       *>>| fun smbuild ->
+       (Lib.name lib, List.map ~f:Lib.name (Smbuild.libraries smbuild))))
+  *>>| String.Table.of_alist_exn
+;;
+
 let link_exe_rule ~dir ~libraries ~external_libraries ~exe names =
-  let include_dirs = Liblinks.include_dirs ~dir libraries in
   let link_exe =
     Dep.all_unit (compiled_files_for ~dir names)
     *>>= fun () ->
+    transitive_libraries ~libraries
+    *>>= fun libraries ->
+    library_deps ~libraries
+    *>>= fun lib_deps ->
+    let libraries =
+      let targets = String.Set.of_list (Hashtbl.keys lib_deps) in
+      List.map ~f:Lib.of_name (toposort ~targets lib_deps)
+    in
     Liblinks.deps libraries ~suffixes:[".cmxa"; ".a"]
     *>>= fun () ->
     toposort_deps ~dir (List.map names ~f:(fun name -> name ^ ".cmx"))
     *>>| fun cmxs ->
-    ocamlopt ~dir ~external_libraries ~for_pack:None ~include_dirs
+    ocamlopt ~dir ~external_libraries ~for_pack:None
+      ~include_dirs:(Liblinks.include_dirs ~dir libraries)
       ~args:(List.concat
                [ [ "-linkpkg"
                  ; "-o"; basename exe
                  ]
-               ; List.map libraries ~f:(fun lib -> lib ^ ".cmxa")
+               ; List.map libraries ~f:(fun lib -> Lib.suffixed lib ^ ".cmxa")
                ; cmxs
                ])
   in
@@ -225,10 +309,10 @@ let link_exe_rule ~dir ~libraries ~external_libraries ~exe names =
 ;;
 
 let link_lib_rules ~dir ~external_libraries ~lib_cmxa ~lib names =
-  let lib_a = dir ^/ lib ^ ".a" in
-  let lib_o = dir ^/ lib ^ ".o" in
-  let lib_cmx = dir ^/ lib ^ ".cmx" in
-  let lib_cmi = dir ^/ lib ^ ".cmi" in
+  let lib_a = dir ^/ Lib.suffixed lib ^ ".a" in
+  let lib_o = dir ^/ Lib.suffixed lib ^ ".o" in
+  let lib_cmx = dir ^/ Lib.suffixed lib ^ ".cmx" in
+  let lib_cmi = dir ^/ Lib.suffixed lib ^ ".cmi" in
   let link_cmxa =
     Dep.path lib_cmx
     *>>| fun () ->
@@ -333,8 +417,8 @@ let lib_rules ~dir =
   *>>= fun smbuild ->
   let external_libraries = Smbuild.external_libraries smbuild in
   let libraries = Smbuild.libraries smbuild in
-  let lib = basename dir ^ "_lib" in
-  let lib_cmxa = dir ^/ lib ^ ".cmxa" in
+  let lib = Lib.of_name (basename dir) in
+  let lib_cmxa = dir ^/ Lib.suffixed lib ^ ".cmxa" in
   compile_mls_in_dir_rules ~dir ~libraries ~external_libraries ~for_pack:(Some lib)
   *>>| fun (names, compile_mls_rules) ->
   List.concat
@@ -345,7 +429,7 @@ let lib_rules ~dir =
 ;;
 
 let liblinks_rules ~dir =
-  Dep.return (Liblinks.rules ~lib:(basename dir))
+  Dep.return (Liblinks.rules ~lib:(Lib.of_suffixed_name (basename dir)))
 ;;
 
 let nothing_to_build_rules ~dir =
