7d4e94eb56bf1aab19a04c7e556fe61c20f2f5b3
Alexandru Scvortov <scvalex@gmail.com>
2014-11-21 22:53:10 +0000

    actually extract vertex indices from Collada

--- lib/res/collada.ml
+++ lib/res/collada.ml
@@ -1,5 +1,14 @@
-open Core.Std let _ = _squelch_unused_module_warning_
+open Core.Std
 open Async.Std
+
+let split_words text =
+  List.filter_map (String.split_on_chars text ~on:[' '; '\t'; '\n'; '\r'])
+    ~f:(fun word ->
+      let word = String.strip word in
+      if String.is_empty word
+      then None
+      else Some word)
+;;
 
 let extract_mesh ~source ~source_id =
   let module X = Xml_helper in
@@ -26,6 +35,19 @@ let extract_mesh ~source ~source_id =
       | None    -> failwithf "can't handle source_id '%s'" xpath ()
       | Some id -> id
     in
+    let polylist_xml =
+      let matches =
+        List.filter (X.matches mesh_xml Q.(empty +> tag "polylist"))
+          ~f:(fun polylist_xml ->
+            X.exists polylist_xml
+              Q.( empty
+                  +> with_attr (tag "input") "semantic" "VERTEX") )
+      in
+      match matches with
+      | [polylist_xml] -> polylist_xml
+      | _              -> failwithf "wrong number of polylists: %d"
+                            (List.length matches) ()
+    in
     let positions_xml =
       X.match_one_exn ~here:_here_ mesh_xml
         Q.( empty
@@ -36,22 +58,26 @@ let extract_mesh ~source ~source_id =
     let positions =
       let float_array_xml =
         X.match_one_exn ~here:_here_ positions_xml
-          Q.( empty
-              +> tag "float_array" )
+          Q.(empty +> tag "float_array")
       in
       let text = X.text_content_exn ~here:_here_ float_array_xml in
-      let words = String.split_on_chars text ~on:[' '; '\t'; '\n'; '\r'] in
-      List.filter_map words ~f:(fun word ->
-        let word = String.strip word in
-        if String.is_empty word
-        then None
-        else Some (Float.of_string word))
+      let words = split_words text in
+      List.map words ~f:Float.of_string
     in
     let positions =
       Float_array.of_array (Array.of_list positions)
     in
     let indices =
-      Int_array.of_array (Array.of_list [])
+      let p_xml =
+        X.match_one_exn ~here:_here_ polylist_xml
+          Q.(empty +> tag "p")
+      in
+      let text = X.text_content_exn ~here:_here_ p_xml in
+      let words = split_words text in
+      List.map words ~f:Int32.of_string
+    in
+    let indices =
+      Int_array.of_array (Array.of_list indices)
     in
     (positions, indices))
 ;;
