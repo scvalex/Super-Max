8ae0bc763f6b4d0b8a545836c37baac954305540
Alexandru Scvortov <scvalex@gmail.com>
2014-11-22 00:08:42 +0000

    better node identification in collada

--- lib/res/collada.ml
+++ lib/res/collada.ml
@@ -1,6 +1,9 @@
 open Core.Std
 open Async.Std
 
+module X = Xml_helper
+module Q = Xml_helper.Query
+
 let split_words text =
   List.filter_map (String.split_on_chars text ~on:[' '; '\t'; '\n'; '\r'])
     ~f:(fun word ->
@@ -10,17 +13,37 @@ let split_words text =
       else Some word)
 ;;
 
+let get_id ~here xml attr =
+  let xpath = X.attr_exn ~here xml attr in
+  match String.chop_prefix xpath ~prefix:"#" with
+  | None    -> failwithf "can't handle attr %s: '%s'" attr xpath ()
+  | Some id -> id
+;;
+
 let extract_mesh ~source ~source_id =
-  let module X = Xml_helper in
-  let module Q = Xml_helper.Query in
   Deferred.Or_error.try_with (fun () ->
     Xml_helper.parse_file_exn source
     >>| fun xml ->
+    let node_xml =
+      X.match_one_exn ~here:_here_ xml
+        Q.( empty
+            +> tag "library_visual_scenes"
+            +> tag "visual_scene"
+            +> with_attr (tag "node") "id" source_id )
+    in
+    (* CR scvalex: Apply tranformation matrix  to vertices. *)
+    let mesh_id =
+      let instance_geometry_xml =
+        X.match_one_exn ~here:_here_ node_xml
+          Q.( empty +> tag "instance_geometry")
+      in
+      get_id ~here:_here_ instance_geometry_xml "url"
+    in
     let mesh_xml =
       X.match_one_exn ~here:_here_ xml
         Q.( empty
             +> tag "library_geometries"
-            +> with_attr (tag "geometry") "id" source_id
+            +> with_attr (tag "geometry") "id" mesh_id
             +> tag "mesh")
     in
     let position_xml =
@@ -29,24 +52,22 @@ let extract_mesh ~source ~source_id =
             +> tag "vertices"
             +> with_attr (tag "input") "semantic" "POSITION" )
     in
-    let source_id =
-      let xpath = X.attr_exn ~here:_here_ position_xml "source" in
-      match String.chop_prefix xpath ~prefix:"#" with
-      | None    -> failwithf "can't handle source_id '%s'" xpath ()
-      | Some id -> id
+    let source_id = get_id ~here:_here_ position_xml "source" in
+    let material_id =
+      let instance_material_xml =
+        X.match_one_exn ~here:_here_ node_xml
+          Q.( empty
+              +> tag "instance_geometry"
+              +> tag "bind_material"
+              +> tag "technique_common"
+              +> tag "instance_material" )
+      in
+      get_id ~here:_here_ instance_material_xml "target"
     in
     let polylist_xml =
-      let matches =
-        List.filter (X.matches mesh_xml Q.(empty +> tag "polylist"))
-          ~f:(fun polylist_xml ->
-            X.exists polylist_xml
-              Q.( empty
-                  +> with_attr (tag "input") "semantic" "VERTEX") )
-      in
-      match matches with
-      | [polylist_xml] -> polylist_xml
-      | _              -> failwithf "wrong number of polylists: %d"
-                            (List.length matches) ()
+      X.match_one_exn ~here:_here_ mesh_xml
+        Q.( empty
+            +> with_attr (tag "polylist") "material" material_id )
     in
     let positions_xml =
       X.match_one_exn ~here:_here_ mesh_xml
@@ -79,7 +100,6 @@ let extract_mesh ~source ~source_id =
       let indices = List.map words ~f:Int32.of_string in
       List.filteri indices ~f:(fun idx _ -> Int.(idx mod input_count = 0))
     in
-    (* CR scvalex: Extract based on node, not on mesh. *)
     let indices =
       Int_array.of_array (Array.of_list indices)
     in
